8db0095f6dd81120b4c92a650a977e419dd58a2b
Minsung <kms3700@gmail.com>
2022-08-02 19:34:11 +0900

    new

--- main.go
+++ main.go
@@ -9,12 +9,23 @@ import (
 
 const port string = ":4000"
 
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
 type URLDescription struct {
-	URL         string `json:"url"`
+	URL         URL    `json:"url"`
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"`
 }
+
+// func (u URLDescription) String() string {
+// 	return "Hello I'm URLDescription Print"
+// }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
@@ -24,9 +35,10 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/",
-			Method:      "GET",
-			Description: "See Documentation",
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
 		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
@@ -38,6 +50,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	http.HandleFunc("/", documentation)
 	fmt.Printf("Listening on http://localhost%s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
