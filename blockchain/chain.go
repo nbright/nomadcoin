e72c163ea876411e63d5f358ffd194238b2da61b
nbright <kms3700@gmail.com>
2022-08-17 21:45:02 +0900

    go

---
+++ blockchain/chain.go
@@ -0,0 +1,37 @@
+package blockchain
+
+import (
+	"sync"
+)
+
+// func (b *BlockChain) listBlocks() {
+// 	for _, block := range b.blocks {
+// 		fmt.Printf("Data: %s\n", block.data)
+// 		fmt.Printf("Hash: %s\n", block.hash)
+// 		fmt.Printf("PreHash: %s\n", block.prevHash)
+// 	}
+// }
+
+type blockChain struct {
+	NewestHash string `json:"newestHash"`
+	Height     int    `json:"height"`
+}
+
+var b *blockChain
+var once sync.Once
+
+func (b *blockChain) AddBlock(data string) {
+	block := createBlock(data, b.NewestHash, b.Height)
+	b.NewestHash = block.Hash
+	b.Height = block.Height
+}
+
+func BlockChain() *blockChain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockChain{"", 0}
+			b.AddBlock("Genesis")
+		})
+	}
+	return b
+}
--- db/db.go
+++ db/db.go
@@ -18,7 +18,6 @@ func DB() *bolt.DB {
 		dbPointer, err := bolt.Open(dbName, 0600, nil)
 		db = dbPointer
 		utils.HandleErr(err)
-
 		err = db.Update(func(t *bolt.Tx) error {
 			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
 			utils.HandleErr(err)
@@ -29,4 +28,22 @@ func DB() *bolt.DB {
 	}
 	return db
 
+}
+
+func SaveBlock(hash string, data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
+		err := bucket.Put([]byte(hash), data)
+		return err
+	})
+	utils.HandleErr(err)
+}
+
+func SaveBlockchain(data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		err := bucket.Put([]byte("checkpoint"), data)
+		return err
+	})
+	utils.HandleErr(err)
 }
