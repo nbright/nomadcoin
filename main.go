0684685de5d262eaf2e706d5e50e51da40bea088
horu2day <horu2day@gmail.com>
2022-09-27 06:30:23 +0900

    chan

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-	"github/nbright/nomadcoin/cli"
-	"github/nbright/nomadcoin/db"
+	"fmt"
+	"time"
 )
 
+func countToTen(c chan int) {
+	for i := range [10]int{} {
+		time.Sleep(10 * time.Second)
+		c <- i
+
+	}
+}
+
 func main() {
-	//go explorer.Start(3000)
-	//rest.Start(4000)
+	// //go explorer.Start(3000)
+	// //rest.Start(4000)
+	// //fmt.Println(os.Args[2:])
+	// //db.DB()
+	// // blockchain.BlockChain().AddBlock("First")
+	// // blockchain.BlockChain().AddBlock("Second")
+	// // blockchain.BlockChain().AddBlock("Third")
+	// defer db.Close()
+	// cli.Start()
+	// //wallet.Wallet()
+	c := make(chan int)
+	go countToTen(c)
+	fmt.Println("blocking")
+	a := <-c //blocking 기다리고 있음.
+	fmt.Println("unblocked")
+	fmt.Println(a)
 
-	//fmt.Println(os.Args[2:])
-
-	//db.DB()
-
-	// blockchain.BlockChain().AddBlock("First")
-	// blockchain.BlockChain().AddBlock("Second")
-	// blockchain.BlockChain().AddBlock("Third")
-
-	defer db.Close()
-	cli.Start()
-
-	//wallet.Wallet()
 }
